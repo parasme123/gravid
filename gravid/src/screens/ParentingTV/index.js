016fb98cba66ad6295dc8565f29c6f7bf6f0204e
jitendra <jitendra.gothwal@parasmesoft.com>
2023-03-08 15:24:25 +0530

    parenting TV video issue

--- gravid/src/screens/ParentingTV/index.js
+++ gravid/src/screens/ParentingTV/index.js
@@ -5,9 +5,11 @@ import styles from "./styles";
 import { svgs, colors } from '@common';
 import VideoPlayer from 'react-native-video-controls';
 import { imageurl } from '../../Services/constants';
+import Video from 'react-native-video';
 
 const ParentingTV = (props) => {
     const playerRef = useRef(null)
+    const videoUrl = 'https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4';
     return (
         <View style={styles.container}>
             <View style={styles.haddingView}>
@@ -18,14 +20,24 @@ const ParentingTV = (props) => {
                 <View style={{ flex: 3 }} />
             </View>
             <View style={styles.radiusView} />
-            <ScrollView style={{ paddingHorizontal: 16 }} showsVerticalScrollIndicator={false}>
-                <VideoPlayer
-                    source={{ uri: imageurl + 'public\vedio\Tusshar Kapoor 7th Cut.mov' }}
+            {/* <Video
+                source={{ uri: videoUrl }}
+                resizeMode="contain"
+                style={{ width: 250, height: 200 }}
+                controls
+            /> */}
+            <VideoPlayer
+                    source={{ uri: 'https://rasatva.apponedemo.top/gravid/public/vedio/Tusshar_Kapoor_7th.mp4', initOptions: ['--codec=avcodec'] }}
+                    // source={{ uri: imageurl + 'public\vedio\Tusshar Kapoor 7th Cut.mov' }}
                     onPause={() => console.log("pause")}
-                    onPlay={() => console.log("onPlay")}
+                    onPlay={() => console.log("Play")}
                     navigator={props.navigator}
+                    onError={err => console.log("err", err)}
+                    style={{ width: "100%", height: 100 }}
                 />
-            </ScrollView>
+            {/* <ScrollView style={{ paddingHorizontal: 16 }} showsVerticalScrollIndicator={false}>
+                
+            </ScrollView> */}
         </View>
     )
 }
